dbe9395110822f1d08fdc7d31c8f9e76a20d65d5
oliverfree <2375828407@qq.com>
2018-07-04 20:58:03 +0800

    modify settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,10 +127,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'downloads'),
 )
 
 STATICFILES_FINDERS = (
